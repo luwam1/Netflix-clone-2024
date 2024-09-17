c3ff692a331606558f98c4c9b08e40e30dda4afa
luwam1 <luwamwel23@gmail.com>
2024-09-17 15:26:16 -0400

    Header and Footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from "./pages/Home/Home"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
-       <h1>hello</h1>
+     <Home/>
+      
     </>
   )
 }
